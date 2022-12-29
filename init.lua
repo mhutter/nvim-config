fa88d19856d4f42da34d413191e3649cfa8337ea
Manuel Hutter <manuel@hutter.io>
2022-12-29 20:47:10 +0100

    Enable wrap
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- init.lua
+++ init.lua
@@ -46,7 +46,7 @@ local config = {
       number = true, -- sets vim.opt.number
       spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = false, -- sets vim.opt.wrap
+      wrap = true, -- sets vim.opt.wrap
       guifont = "Fira Code:h11:e-subpixelantialias",
       termguicolors = true,
     },
