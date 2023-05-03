57d23efec733c4c6c81fe361713cd2b14d88741d
Manuel Hutter <manuel@hutter.io>
2023-05-03 08:01:35 +0200

    Restructure repo
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- init.lua
+++ init.lua
@@ -40,46 +40,17 @@ return {
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
     },
-
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
-    },
-  },
-
-  plugins = {
-    { "sainnhe/sonokai" },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = {
-          "cssls",
-          "gopls",
-          "jsonnet_ls",
-          "ltex",
-          "rust_analyzer",
-          "terraformls",
-        },
-      },
-    },
-    {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function()
-        require("copilot").setup({
-          suggestion = {
-            auto_trigger = true,
-          },
-        })
-      end,
     },
   },
 
@@ -89,7 +60,7 @@ return {
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
+        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
       },
     },
   },
@@ -118,6 +89,7 @@ return {
       },
       filename = {
         [".envrc"] = "sh",
+        ["Justfile"] = "make",
       },
     }
 
