6552cec4c8ae1f859e30e58e83b904a21efc6798
Manuel Hutter <manuel@hutter.io>
2023-10-10 09:07:44 +0200

    Copilot: Only enable selectively
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -7,6 +7,23 @@ return {
       suggestion = {
         auto_trigger = true,
       },
+      filetypes = {
+        -- Enable for these filetypes
+        go = true,
+        make = true,
+        rust = true,
+
+        -- For shellscripts, only enable if it's not an .env file
+        sh = function ()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '%.env') then
+            return false
+          end
+          return true
+        end,
+
+        -- Disable for everything else
+        ["*"] = false,
+      },
     })
   end,
 }
