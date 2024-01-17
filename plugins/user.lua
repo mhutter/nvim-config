9a63ce05adc5e92e9147b2f08d2d3fbd3dae0e24
Manuel Hutter <manuel@hutter.io>
2024-01-17 14:09:31 +0100

    Add user snippets
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,4 +16,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        -- this can be used if your configuration lives in ~/.config/nvim
+        -- if your configuration lives in ~/.config/astronvim, the full path
+        -- must be specified in the next line
+        paths = { "./lua/user/snippets" }
+      }
+    end,
+  },
 }
